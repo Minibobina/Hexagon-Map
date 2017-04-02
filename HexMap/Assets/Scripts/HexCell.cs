d644997e6ebfc390871bde9781685ac5cc2862cb
Colin Menz <colinmenz@mail.usf.edu>
2017-04-02 03:28:28 -0400

    Last update for 4/1/17

--- HexMap/Assets/Scripts/HexCell.cs
+++ HexMap/Assets/Scripts/HexCell.cs
@@ -5,4 +5,18 @@ public class HexCell : MonoBehaviour {
 	public HexCoordinates coordinates;
 
 	public Color color;
+
+	[SerializeField]
+	HexCell[] neighbors;
+
+	public HexCell GetNeighbor (HexDirection dir)
+	{
+		return neighbors[(int)dir];
+	}
+	
+	public void SetNeighbor (HexDirection dir, HexCell cell)
+	{
+		neighbors[(int)dir] = cell;
+		cell.neighbors[(int)dir.Opposite()] = this;
+	}
 }
